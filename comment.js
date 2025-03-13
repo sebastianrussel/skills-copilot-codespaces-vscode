6927ed19b5583be0573321b7d8529d834ec18d0e
sebastianrussel <153105665+sebastianrussel@users.noreply.github.com>
2025-03-13 11:48:24 +0000

    comment.js

--- comment.js
+++ comment.js
@@ -1,62 +1,65 @@
 //create web server
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
-var template = require('./lib/template.js');
-var path = require('path');
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
+var qs = require("querystring");
 
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
-    if(pathname === '/'){
-        if(queryData.id === undefined){
-            fs.readdir('./data', function(error, filelist){
-                var title = 'Welcome';
-                var description = 'Hello, Node.js';
-                var list = template.list(filelist);
-                var html = template.HTML(title, list,
-                    `<h2>${title}</h2>${description}`,
-                    `<a href="/create">create</a>`
-                );
-                response.writeHead(200);
-                response.end(html);
-            });
-        } else {
-            fs.readdir('./data', function(error, filelist){
-                var filteredId = path.parse(queryData.id).base;
-                fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-                    var title = queryData.id;
-                    var list = template.list(filelist);
-                    var html = template.HTML(title, list,
-                        `<h2>${title}</h2>${description}`,
-                        `<a href="/create">create</a>
-                        <a href="/update?id=${title}">update</a>
-                        <form action="delete_process" method="post">
-                            <input type="hidden" name="id" value="${title}">
-                            <input type="submit" value="delete">
-                        </form>`
-                    );
-                    response.writeHead(200);
-                    response.end(html);
-                });
-            });
-        }
-    } else if(pathname === '/create'){
-        fs.readdir('./data', function(error, filelist){
-            var title = 'WEB - create';
-            var list = template.list(filelist);
-            var html = template.HTML(title, list, `
-                <form action="/create_process" method="post">
-                    <p><input type="text" name="title" placeholder="title"></p>
-                    <p>
-                        <textarea name="description" placeholder="description"></textarea>
-                    </p>
-                    <p>
-                        <input type="submit">
-                    </p>
-                </form>
-            `, '');
-            response.writeHead(200);
-            response.end(html
+var template = {
+  HTML: function (title, list, body, control) {
+    return `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        ${list}
+        ${control}
+        ${body}
+      </body>
+      </html>
+    `;
+  },
+  list: function (filelist) {
+    var list = "<ul>";
+    for (var i = 0; i < filelist.length; i++) {
+      list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    }
+    list += "</ul>";
+    return list;
+  },
+};
+
+var app = http.createServer(function (req, res) {
+  var _url = req.url;
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
+  if (pathname === "/") {
+    if (queryData.id === undefined) {
+      fs.readdir("./data", function (err, filelist) {
+        var title = "Welcome";
+        var description = "Hello, Node.js";
+        var list = template.list(filelist);
+        var html = template.HTML(
+          title,
+          list,
+          `<h2>${title}</h2>${description}`,
+          `<a href="/create">create</a>`
+        );
+        res.writeHead(200);
+        res.end(html);
+      });
+    } else {
+      fs.readdir("./data", function (err, filelist) {
+        fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
+          var title = queryData.id;
+          var list = template.list(filelist);
+          var html = template.HTML(
+            title,
+            list,
+            `<h2>${title}</h2>${description}`,
+            `<a href="/create">create</a> <a href="/update?id=${title}">update</a> <form action="/delete_process" method="post">
+            <input type="hidden" name="id" value="${title}">
+            <input type="submit
