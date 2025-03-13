7f944d04e81dea077f9a5f1e9cbf5549871f7be4
sebastianrussel <153105665+sebastianrussel@users.noreply.github.com>
2025-03-13 12:15:50 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,9 +1,3 @@
 function skillsMember() {
-  var member = Member.findOne({userId: Meteor.userId()});
-  if (member) {
-    if (member.skills) {
-      return member.skills;
-    }
-  }
-  return [];
+  console.log('I am a member');
 }
